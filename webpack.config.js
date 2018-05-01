60c617dcdd04c9d4aaa9e73c2ea01552aa6f3be5
Aaron Sisler <asisler@corp.epnet.com>
2018-05-01 14:13:25 -0400

    Adding in the production change

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,33 @@
 const path = require('path');
 
-module.exports = {
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    },
-    devtool: 'cheap-module-eval-source-map',
-    entry: './src/app.js',
-    // entry: './src/playground/hoc.js',
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        entry: './src/app.js',
+        // entry: './src/playground/hoc.js',
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js',
+        }
     }
-};
+}
