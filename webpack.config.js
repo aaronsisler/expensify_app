0aae633e351f8c2913dcbee472cb2675a93d1e6a
Aaron Sisler <asisler@corp.epnet.com>
2018-05-01 14:43:59 -0400

    Adding in the CSS prod changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
     const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
 
     return {
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true
         },
-        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
         entry: './src/app.js',
         // entry: './src/playground/hoc.js',
         module: {
@@ -18,16 +20,30 @@ module.exports = (env) => {
                 exclude: /node_modules/
             }, {
                 test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
             }]
         },
         output: {
             path: path.join(__dirname, 'public'),
             filename: 'bundle.js',
-        }
+        },
+        plugins: [
+            CSSExtract
+        ]
     }
 }
