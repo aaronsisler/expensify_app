3adac30107dcc17505da5c30122b3bb9f846568e
Aaron Sisler <asisler@corp.epnet.com>
2018-05-06 17:19:23 -0400

    Changes for Remove Expense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,4 +1,3 @@
-import uuid from 'uuid';
 import database from '../firebase/firebase';
 
 export const addExpense = expense => ({
@@ -30,7 +29,16 @@ export const startAddExpense = (expenseData = {}) => {
 export const removeExpense = id => ({
     type: 'REMOVE_EXPENSE',
     id
-})
+});
+
+export const startRemoveExpense = id => {
+    console.log('Id: ', id);
+    return dispatch => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense(id));
+        })
+    };
+}
 
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
