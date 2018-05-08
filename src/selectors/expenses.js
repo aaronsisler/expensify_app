e31b04ced96e8a53d722bf5f3710f2f7f7eddac9
Aaron Sisler <asisler@corp.epnet.com>
2018-05-08 13:56:12 -0400

    Adding in the styling

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,6 +1,8 @@
 import moment from 'moment';
 
-const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+// Get visible expenses
+
+export default (expenses, { text, sortBy, startDate, endDate }) => {
     return expenses.filter((expense) => {
         const createdAtMoment = moment(expense.createdAt);
         const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
@@ -11,11 +13,8 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
     }).sort((a, b) => {
         if (sortBy === 'date') {
             return a.createdAt < b.createdAt ? 1 : -1;
-        }
-        else if (sortBy === 'amount') {
+        } else if (sortBy === 'amount') {
             return a.amount < b.amount ? 1 : -1;
         }
     });
-}
-
-export default getVisibleExpenses;
+};
